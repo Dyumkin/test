a0acb626ef2eb5f17f110950b744476b115e7751
Yury Dyumkin <y.dyumkin@itransition.com>
2014-01-17 11:09:45 +0300

    test_commite

--- protected/components/views/_userForm.php
+++ protected/components/views/_userForm.php
@@ -3,54 +3,58 @@
 /* @var $model User */
 /* @var $form TbActiveForm */
 ?>
-    <?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-        'id' => 'horizontalForm',
-        'type' => 'horizontal',
-        'enableClientValidation' => true,
-        'htmlOptions' => array('enctype'=>'multipart/form-data'),
-    )); ?>
+<?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id' => 'horizontalForm',
+    'type' => 'horizontal',
+    'enableClientValidation' => true,
+    'htmlOptions' => array('enctype' => 'multipart/form-data'),
+)); ?>
 
-    <p class="note">Fields with <span class="required">*</span> are required.</p>
+<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-    <?php echo $form->errorSummary($model); ?>
+<?php echo $form->errorSummary($model); ?>
 
-        <?php echo $form->textFieldRow($model, 'username'); ?>
+<?php $this->widget('ext.imageAttachment.ImageAttachmentWidget', array(
+    'model' => $model,
+    'behaviorName' => 'preview',
+    'apiRoute' => 'user/saveImageAttachment',
+)); ?>
 
-        <?php echo $form->textFieldRow($model, 'e_mail', array('prepend' => '@')); ?>
+<?php echo $form->textFieldRow($model, 'username'); ?>
 
-        <?php echo $form->textFieldRow($model, 'name', array('size' => 60, 'maxlength' => 60)); ?>
+<?php echo $form->textFieldRow($model, 'e_mail', array('prepend' => '@')); ?>
 
-        <?php echo $form->textFieldRow($model, 'first_name', array('size' => 60, 'maxlength' => 60)); ?>
+<?php echo $form->textFieldRow($model, 'name', array('size' => 60, 'maxlength' => 60)); ?>
 
-        <?php echo $form->textFieldRow($model, 'last_name', array('size' => 60, 'maxlength' => 60)); ?>
+<?php echo $form->textFieldRow($model, 'first_name', array('size' => 60, 'maxlength' => 60)); ?>
 
-        <?php echo $form->dropDownListRow($model, 'sex',$model->genderOptions,array('empty' => 'Select a gender') ); ?>
+<?php echo $form->textFieldRow($model, 'last_name', array('size' => 60, 'maxlength' => 60)); ?>
 
-        <?php $this->widget('WidgetProvider', array('model' => $model)); ?>
+<?php echo $form->dropDownListRow($model, 'sex', $model->genderOptions, array('empty' => 'Select a gender')); ?>
 
-        <?php echo $form->datepickerRow($model,'birthday',array(
-            'prepend'=>'<i class="icon-calendar"></i>',
-            'options' => array(
-                'format' => 'dd.mm.yyyy',
-                'language' => 'ru',
-                'autoclose'=>'true',
-                'startDate'=>'1,1,1920',
-                'endDate'=>'now()',
-                'startView'=>2,
-            ),
+<?php $this->widget('WidgetProvider', array('model' => $model)); ?>
 
-        )); ?>
+<?php echo $form->datepickerRow($model, 'birthday', array(
+    'prepend' => '<i class="icon-calendar"></i>',
+    'options' => array(
+        'format' => 'dd.mm.yyyy',
+        'language' => 'ru',
+        'autoclose' => 'true',
+        'startDate' => '1,1,1920',
+        'endDate' => 'now()',
+        'startView' => 2,
+    ),
 
+));
+?>
 
+<?php echo $form->textFieldRow($model, 'phone', array('size' => 18, 'maxlength' => 18)); ?>
 
-        <?php echo $form->textFieldRow($model, 'phone', array('size' => 18, 'maxlength' => 18)); ?>
+<?php echo $form->textAreaRow($model, 'other_information', array('rows' => 6, 'cols' => 50)); ?>
 
-        <?php echo $form->textAreaRow($model, 'other_information', array('rows' => 6, 'cols' => 50)); ?>
+<?php echo TbHtml::formActions(array(
+    TbHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('color' => TbHtml::BUTTON_COLOR_PRIMARY)),
+    TbHtml::resetButton('Reset'),
+)); ?>
 
-    <?php echo TbHtml::formActions(array(
-        TbHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('color' => TbHtml::BUTTON_COLOR_PRIMARY)),
-        TbHtml::resetButton('Reset'),
-    )); ?>
-
-    <?php $this->endWidget(); ?>
-
+<?php $this->endWidget(); ?>
